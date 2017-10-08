2996112f9a72a3c47cdbcffc05b6aac0d3aa87e4
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-10-08 12:59:28 +0000

    making huge progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Infostack - The only forum you'll ever need"
+    base_title = "Infostack - Giving the world a voice."
     page_title.empty? ? base_title : "Infostack - #{page_title}"
   end
 end
